a740812613270587047984455ae927fc039a9121
root <root@EsToDa.localdomain>
2024-07-24 17:10:59 +0300

    done

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "AhmedAmin@82253",
+    database = "",
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+print("Database 'alx_book_store' created successfully!")
+
+
+mycursor.close()
+mydb.close()
