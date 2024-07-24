f7b7b5460903736504956e2395141fb69eb67ffb
root <root@EsToDa.localdomain>
2024-07-24 18:39:17 +0300

    don

---
+++ task_6.sql
@@ -0,0 +1,22 @@
+USE alx_book_store;
+
+INSERT INTO Customers(customer_id, customer_name, email, address)
+VALUES
+    (
+        2,
+        'Blessing Malik',
+        'bmalik@sandtech.com',
+        '123 Happiness  Ave.'
+    ),
+    (
+        3,
+        'Obed Ehoneah',
+        'eobed@sandtech.com',
+        '123 Happiness  Ave.'
+    ),
+    (
+        4,
+        'Nehemial Kamolu',
+        'nkamolu@sandteck.com',
+        '123 Happiness  Ave.'
+    );
